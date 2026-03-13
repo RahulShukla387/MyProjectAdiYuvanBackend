e91abb25c4ddf98cab5a9ae469fa030032290796
RahulShukla387 <KnowMeTechnical@gmail.com>
2026-03-14 00:39:01 +0530

    adding origin

--- config/Cors.js
+++ config/Cors.js
@@ -2,7 +2,8 @@
  
 let UseCors =()=>{
      return {
-        origin: "http://localhost:5173",
+        // origin: "http://localhost:5173",
+        origin: "https://myprojectadiyuvanbackend-production.up.railway.app",
         methods: ["GET", "POST", "PATCH", "PUT", "DELETE" ],
          allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
